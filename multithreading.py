dbf35b9d8fa1a873b9c92ddc58b8a0f9873d2671
Zinkton <paulius.trub@gmail.com>
2024-01-06 00:14:34 +0200

    hardcode trainer optimizations

--- multithreading.py
+++
@@ -1,6 +0,0 @@
-import constants
-from multiprocessing import Pool
-
-def multithreading_pool(function, inputs):
-    with Pool(processes=constants.THREADS) as p:
-        return p.map(function, inputs)
